f6d52f5472d170b5f55883c6f630ca04d3145b23
kingofcrabs <rex.xie@qq.com>
2015-09-20 18:18:12 +0800

    created

---
+++ OptimizePooling/OptimizePooling/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptimizePooling
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetGridCnt_Click(object sender, EventArgs e)
+        {
+           
+            string sGridCnt = txtGridCnt.Text;
+            if (sGridCnt == "")
+            {
+                SetErrorInfo("样本数不得为空！");
+                return;
+            }
+            int gridCnt = 0;
+            bool bOk = int.TryParse(sGridCnt, out gridCnt);
+            if (gridCnt < 1 || gridCnt > 10)
+            {
+                SetErrorInfo("样本数必须在1~10之间！");
+                return;
+            }
+           
+            EnableControls(false);
+
+        }
+
+        private void SetErrorInfo(string info)
+        {
+            txtInfo.Text = info;
+            txtInfo.ForeColor = Color.Red;
+            txtInfo.BackColor = Color.White;
+        }
+
+        private void EnableControls(bool bEnable)
+        {
+            txtGridCnt.Enabled = bEnable;
+        }
+    }
+}
---
+++ OptimizePooling/OptimizePooling/worklist.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizePooling
+{
+    class worklist
+    {
+        int curPlateColumnIndex = 0;
+        public List<string> Generate(int sampleCount)
+        {
+            int sampleCntPerBatch = 8 * GlobalVars.Instance.PoolingCnt;
+            int batchCnt = sampleCount / sampleCntPerBatch;
+            List<PipettingInfo> pipettingInfos = new List<PipettingInfo>();
+            int startGridID = int.Parse(ConfigurationManager.AppSettings["startGrid"]);
+            for (int i = 0; i < batchCnt; i++)
+            {
+                pipettingInfos.AddRange(GenerateBatch(startGridID));
+                curPlateColumnIndex++;
+                startGridID += 3;
+            }
+            List<string> strs = Format(pipettingInfos);
+            strs.Add("B");
+            int remainingCnt = sampleCount - batchCnt * sampleCntPerBatch;
+            int dstWellCntNeeded = remainingCnt / GlobalVars.Instance.PoolingCnt;
+
+
+            return strs;
+        }
+
+        private List<string> Format(List<PipettingInfo> pipettingInfos)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<PipettingInfo> GenerateBatch(int startGridID)
+        {
+            List<PipettingInfo> pipettingInfos = new List<PipettingInfo>();
+           
+            for(int iGrid = 0; iGrid < 3; iGrid++)
+            {
+                int srcGrid = startGridID + iGrid;
+                for( int wellIndex = 0; wellIndex < 16 ; wellIndex++)
+                {
+                    int dstWellIndex = wellIndex;
+                    if (dstWellIndex >= 8)
+                        dstWellIndex -= 8;
+                    pipettingInfos.Add(new PipettingInfo(string.Format("grid{0}", srcGrid),
+                        wellIndex + 1,
+                        GlobalVars.Instance.PipettingVolume,
+                        GlobalVars.Instance.DstLabware,
+                        curPlateColumnIndex * 8 + dstWellIndex + 1, GlobalVars.Instance.pos_BarcodeDict[new Position(srcGrid - 1, wellIndex)]));
+                }
+            }
+            return pipettingInfos;
+        }
+
+
+      
+    }
+    struct Position
+    {
+        public int x;
+        public int y;
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class GlobalVars
+    {
+        static GlobalVars instance = null;
+        public Dictionary<Position, string> pos_BarcodeDict;
+        public static GlobalVars Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GlobalVars();
+                return instance;
+            }
+        }
+
+        public  string DstLabware
+        {
+            get{
+                return GetSetting("dstLabware");
+            }
+        }
+
+        public int PoolingCnt
+        {
+            get
+            {
+                return int.Parse(GetSetting("poolingCnt"));
+            }
+        }
+
+
+
+        public double PipettingVolume
+        {
+            get
+            {
+                return double.Parse(GetSetting("volumeUL"));
+            }
+        }
+
+
+        private string GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+        
+    }
+
+    class PipettingInfo
+    {
+        public string srcLabware;
+        public int srcWellID;
+        public double volume;
+        public string dstLabware;
+        public int dstWellID;
+        public string barcode;
+
+        public PipettingInfo(string srcLabware, 
+            int srcWellID, 
+            double volume, 
+            string dstLabware,
+            int dstWellID,
+            string barcode)
+        {
+            this.srcLabware = srcLabware;
+            this.srcWellID = srcWellID;
+            this.volume = volume;
+            this.dstLabware = dstLabware;
+            this.dstWellID = dstWellID;
+            this.barcode = barcode;
+        }
+    }
+}
