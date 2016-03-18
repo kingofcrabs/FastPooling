95f860785764e3dcd5ecb54810a14a7c95a68915
Rex <rex.xie@qq.com>
2016-03-18 09:41:24 +0800

    0.17

--- OptimizePooling/FastPooling/strings.Designer.cs
+++ OptimizePooling/FastPooling/strings.Designer.cs
@@ -70,7 +70,7 @@ namespace FastPooling {
         }
         
         /// <summary>
-        ///   查找类似 0.16 的本地化字符串。
+        ///   查找类似 0.17 的本地化字符串。
         /// </summary>
         internal static string version {
             get {
